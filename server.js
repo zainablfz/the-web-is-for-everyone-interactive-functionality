7bcc8ea948e88f1b1da8e37f1449e3764a1c56d9
zainablfz <144009548+zainablfz@users.noreply.github.com>
2024-04-03 11:37:37 +0200

    POST-route voor delen

--- server.js
+++ server.js
@@ -1,74 +1,62 @@
-// Stap 1 | Importeren van de bestanden 
-
-// Importeer het npm pakket express uit de node_modules map
 import express from 'express'
-
-// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
-// Maak een nieuwe express app aan
+const redpers_url = 'https://redpers.nl/wp-json/wp/v2/posts'
+const directus_url = 'https://fdnd-agency.directus.app/items/redpers_shares'
+
 const app = express()
-
-// De link naar de data definieren
-const apiUrl = 'https://redpers.nl/wp-json/wp/v2'
-const apiPosts = apiUrl + '/posts'
-const apiUsers = apiUrl + '/users'
-const apiCategories = apiUrl + '/categories'
-
-// Stel ejs in als template engine
 app.set('view engine', 'ejs')
-
-// Stel de map met ejs templates in
 app.set('views', './views')
-
-// De map 'public' staat voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.set('port', process.env.PORT || 8000)
 app.use(express.static('public'))
-
 app.use(express.urlencoded({ extended: true }))
 
-// Stap 3 | Het maken van routes van de server
-
-app.get('/', function (request, response) {
-    fetchJson(apiPosts).then((apiData) => {
-        response.render('home', {
-            articles: apiData
+app.get('/', (request, response) => {
+  fetchJson(redpers_url).then((data) => {
+    fetchJson(directus_url).then((shares) => {
+      // Map de data array uit wordpress, map is een soort 'loop' structuur voor arrays
+      data.map((article) =>
+        // Gebruik de Object.assign() functie om het aantal shares toe te voegen op het article
+        // NB: Object.assign past het 'article' object
+        Object.assign(article, {
+          // Zoek in shares naar article.slug, koppel het aantal shares of 0 als het niet bestaat
+          shares: shares.data.find(({ slug }) => slug == article.slug)?.shares || 0,
         })
+      )
+      response.render('index', { articles: data })
     })
+  })
 })
 
 
-app.get('/artikel/:id', function (request, response) {
-  fetchJson(apiPosts + '?include= +' + request.params.id).then((apiData) => {
+app.get('/article', (request, response) => {
+  response.redirect(301, '/')
+})
 
-      response.render('article', {
-          article: apiData
-      })
-      console.log(apiData)
+app.post('/article/:slug', (request, response) => {
+  fetchJson(`${directus_url}?filter[slug][_eq]=${request.params.slug}`).then(({ data }) => {
+    // Doe een PATCH op directus, stuur de id mee als die er is.
+    fetchJson(`${directus_url}/${data[0]?.id ? data[0].id : ''}`, {
+      method: data[0]?.id ? 'PATCH' : 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        slug: request.params.slug,
+        shares: data.length > 0 ? data[0].shares + 1 : 1,
+      }),
+    }).then((result) => {
+      console.log(result)
     })
   })
+  response.redirect(301, `/article/${request.params.slug}`)
+})
 
-
-
-const categoryRoutes = [
-    'binnenland',
-    'buitenland',
-    'column',
-    'economie',
-    'kunst & media',
-    'podcast',
-    'politiek',
-    'wetenschap'
-  ]
-  
-  categoryRoutes.forEach(category => {
-    app.get("/" + category, function (request, response) {
-        fetchJson(apiCategories).then(([mediaAPI, postsAPI]) => {
-        response.render(categoryRoutes, { media: mediaAPI, posts: postsAPI });
-      });
-    });
+app.get('/article/:slug', (request, response) => {
+  fetchJson(`${redpers_url}?slug=${request.params.slug}`).then((data) => {
+    // fetch de #shares
+    response.render('article', { article: data[0] })
   })
+})
 
-// Stap 5 | Een route geven aan de server om te draaien
 
 app.set('port', process.env.PORT || 8081)
 
